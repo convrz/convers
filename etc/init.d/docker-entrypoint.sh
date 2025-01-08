0d50b2cd2fdcdcc7dcd392c35d43cb1e9a302124
hunghd <iduchungho@gmail.com>
2025-01-08 16:34:56 +0700

    fix: script in etc

--- etc/init.d/docker-entrypoint.sh
+++ etc/init.d/docker-entrypoint.sh
@@ -13,8 +13,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-docker compose \ 
-    -f ./etc/docker/mesh/docker-compose.yml \ 
-    -f ./etc/docker/resource/docker-compose.resources.yml \
-    up
